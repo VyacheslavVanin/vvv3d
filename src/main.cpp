0611dead664eb28d91f3e2e1d2e12360d4b5e285
Vyacheslav.Vanin <vycheslav.vanin@gmail.com>
2016-03-23 06:57:51 +0300

    clear main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,6 @@
 #include <iomanip>
 #include <string>
 
-using namespace std;
-
-template<typename T>
-T randUniform(T low, T high)
-{
-    return low + static_cast <T> (rand()) /( static_cast <T> (RAND_MAX/(high-low)));
-}
-
-float randUniform(){return randUniform(0.0f,1.0f);}
-
 
 int main(int argc, char** argv)
 {
