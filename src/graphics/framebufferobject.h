fcabdfa2053de3ea1aa523eedefcd71167b4fb35
Vyacheslav.Vanin <vycheslav.vanin@gmail.com>
2016-05-11 10:46:45 +0300

    delete copy constructor and assignement for FrameBufferObject

--- src/graphics/framebufferobject.h
+++ src/graphics/framebufferobject.h
@@ -8,6 +8,8 @@ public:
     FrameBufferObject();
 
     ~FrameBufferObject();
+    FrameBufferObject(const FrameBufferObject& other) = delete;
+    FrameBufferObject& operator=(const FrameBufferObject& other) = delete;
 
 /* Example:
    Texture( std::shared_ptr<LowLevelTexture>( new LowLevelTexture(0,1024,1024,GL_RGBA,GL_RGBA,GL_UNSIGNED_BYTE)) ));
