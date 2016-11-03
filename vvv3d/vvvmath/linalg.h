e4c1e39544fe8edefa2744528ca3130a256c2fba
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2016-11-03 18:02:31 +0300

    linalg: add clamp_fast()

--- vvv3d/vvvmath/linalg.h
+++ vvv3d/vvvmath/linalg.h
@@ -806,11 +806,31 @@ namespace vvv
     }
 
     template<typename T>
+    /**
+     * @brief clamp function
+     * @param l1 first limit value
+     * @param l2 second limit value
+     * @param val value to pass
+     * @return value if fits limits else
+     *  if val greater than both limits then return max(l1, l2)
+     *  if val less than both limits then return min(l1, l2) */
     T clamp(const T& l1, const T& l2, const T& val)
     {
         const T& mx = l1 > l2 ? l1 : l2;
         const T& mn = l1 > l2 ? l2 : l1;
         return std::max(std::min(mx, val), mn);
+    }
+
+    template<typename T>
+    /**
+     * @brief clamp_fast same as clamp but min <= max
+     * @param min
+     * @param max
+     * @param val
+     * @return clamped value */
+    T clamp_fast(const T& min, const T& max, const T& val)
+    {
+        return std::max(std::min(max, val), min);
     }
 }
 
