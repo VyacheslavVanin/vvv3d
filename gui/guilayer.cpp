748ff5410d91c016a866a2e03e283e26585afd5a
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2017-05-05 20:40:12 +0300

    remove pimpl from GuiLayer

--- gui/guilayer.cpp
+++ gui/guilayer.cpp
@@ -1,191 +1,71 @@
-#include <vvv3d/vvv3d.h>
-#include <vector>
-#include <queue>
+#include "guilayer.h"
+#include "vvvstlhelper.hpp"
+#include "widget.h"
 #include <algorithm>
 #include <assert.h>
-#include "guilayer.h"
-#include "widget.h"
-#include "vvvstlhelper.hpp"
-
-using WidgetsContainer = std::vector<Widget*>;
-
-class GuiPointer
-{
-public:
-    GuiPointer(WidgetsContainer& widgets)
-        : widgets(widgets)
-    {}
-
-    void processInput(const Input& input);
-
-    Widget* getWidgetAtPoint(const vvv::vector2i& pos);
-
-    constexpr static uint16_t NUM_MOUSE_BUTTONS = 8;
-
-private:
-    WidgetsContainer& widgets;
-    bool    mouseButtonsStates[NUM_MOUSE_BUTTONS] {false};
-    void detectMouseMove(const Input::Mouse& mouse);
-    void detectMouseButtons(const Input::Mouse& mouse);
-    void processKeyboard(const Input::Keyboard& kbd);
-};
-
-struct GuiLayer::GuiLayerImpl
-{
-    WidgetsContainer widgets;
-    vvv::vector2<int>    size;
-    /**
-     * @brief Orthographic camera; top-left corner (0,0)
-     */
-    Camera camera;
-    GuiPointer pointer;
-    bool visible;
-
-    GuiLayerImpl();
-
-    /**
-     * @brief Draw all widgets. */
-    void draw();
-    void resize(int width, int height);
-    void processInputEvents(const Input& input);
-
-    /**
-     * @brief Add widget to layer.
-     *  Container become responsible for deleting widget.
-     * @param widget widget to add. */
-    void addWidget(Widget* widget);
-
-    /**
-     * @brief Remove widget from Layer.
-     *  After this call you responsible for deleting widget.
-     * @param widget widget to remove from this container. */
-    void removeWidget(Widget* widget);
-
-    ~GuiLayerImpl();
-
-public:
-    bool getVisible() const;
-    void setVisible(bool value);
-};
-
-bool GuiLayer::GuiLayerImpl::getVisible() const
-{
-    return visible;
-}
-
-void GuiLayer::GuiLayerImpl::setVisible(bool value)
-{
-    visible = value;
-}
-
-GuiLayer::GuiLayerImpl::GuiLayerImpl()
-    : widgets(), size(), camera(), pointer(this->widgets), visible(false)
-{  }
-
-void GuiLayer::GuiLayerImpl::draw()
-{
-    if(!visible)
-        return;
-
-    DRAW_TRANSPARENT
-    for(auto w: widgets)
-        w->Draw();
-}
-
-void GuiLayer::GuiLayerImpl::resize(int width, int height)
-{
-    camera.setOrtho(0, width, -height, 0, 1, -1);
-    size.set(width, height);
-}
-
-void GuiLayer::GuiLayerImpl::addWidget(Widget* widget)
-{
-    const auto it = std::find(widgets.begin(), widgets.end(), widget);
-    if(it == widgets.end())
-        widgets.push_back(widget);
-}
-
-void GuiLayer::GuiLayerImpl::removeWidget(Widget* widget)
-{
-    const auto it = std::find(widgets.begin(), widgets.end(), widget);
-    if(it != widgets.end())
-        widgets.erase(it);
-}
-
-
-void GuiLayer::GuiLayerImpl::processInputEvents(const Input& input)
-{
-    pointer.processInput(input);
-}
-
-GuiLayer::GuiLayerImpl::~GuiLayerImpl()
-{
-    for(auto w: widgets)
-        delete w;
-}
-
+#include <queue>
+#include <vector>
+#include <vvv3d/vvv3d.h>
 
 GuiLayer::GuiLayer()
-    : impl(std::make_unique<GuiLayerImpl>())
+    : widgets(), size(), camera(), pointer(this->widgets), visible(false)
 {
-
 }
 
 void GuiLayer::draw()
 {
-    impl->draw();
+    if (!visible)
+        return;
+
+    DRAW_TRANSPARENT
+    for (auto w : widgets)
+        w->Draw();
 }
 
 void GuiLayer::addWidget(Widget* widget)
 {
-    impl->addWidget(widget);
+    const auto it = std::find(widgets.begin(), widgets.end(), widget);
+    if (it == widgets.end())
+        widgets.push_back(widget);
     widget->setGuiLayer(this);
 }
 
 void GuiLayer::removeWidget(Widget* widget)
 {
-    impl->removeWidget(widget);
+    const auto it = std::find(widgets.begin(), widgets.end(), widget);
+    if (it != widgets.end())
+        widgets.erase(it);
 }
 
 void GuiLayer::resize(int width, int height)
 {
-    impl->resize(width, height);
+    camera.setOrtho(0, width, -height, 0, 1, -1);
+    size.set(width, height);
 }
 
-const vvv::vector2<int>& GuiLayer::getSize() const
-{
-    return impl->size;
-}
+const vvv::vector2<int>& GuiLayer::getSize() const { return size; }
 
-const Camera& GuiLayer::getCamera() const
-{
-    return impl->camera;
-}
+const Camera& GuiLayer::getCamera() const { return camera; }
 
 void GuiLayer::processInputEvents(const Input& input)
 {
-    if(!getVisible())
+    if (!getVisible())
         return;
-    impl->processInputEvents(input);
+    pointer.processInput(input);
 }
 
-void GuiLayer::setVisible(bool visisble)
+void GuiLayer::setVisible(bool visible) { this->visible = visible; }
+
+bool GuiLayer::getVisible() const { return visible; }
+
+GuiLayer::~GuiLayer()
 {
-    impl->setVisible(visisble);
+    for (auto w : widgets)
+        delete w;
 }
 
-bool GuiLayer::getVisible() const
-{
-    return impl->getVisible();
-}
-
-
-GuiLayer::~GuiLayer() = default;
 GuiLayer::GuiLayer(GuiLayer&&) noexcept = default;
 GuiLayer& GuiLayer::operator=(GuiLayer&&) noexcept = default;
-
-
-
 
 static bool isPointInWidget(const Widget* w, const vvv::vector2i& point)
 {
@@ -196,35 +76,33 @@ static bool isPointInWidget(const Widget* w, const vvv::vector2i& point)
  * @brief Return pointer to widget that contain pos
  * @param pos point to test
  * @return Widget or nullptr if no widgets under point */
-Widget* GuiPointer::getWidgetAtPoint(const vvv::vector2i& pos)
+Widget* GuiLayer::GuiPointer::getWidgetAtPoint(const vvv::vector2i& pos)
 {
-    auto w = std::find_if(widgets.rbegin(), widgets.rend(),
-                          [&pos](const auto w){
-                            return isPointInWidget(w, pos);
-                          });
-    if(w == widgets.rend())
+    auto w =
+        std::find_if(widgets->rbegin(), widgets->rend(),
+                     [&pos](const auto w) { return isPointInWidget(w, pos); });
+    if (w == widgets->rend())
         return nullptr;
     return *w;
 }
 
-
-void GuiPointer::detectMouseMove(const Input::Mouse& mouse)
+void GuiLayer::GuiPointer::detectMouseMove(const Input::Mouse& mouse)
 {
     if (!mouse.isMoved())
         return;
     const auto x = mouse.getMouseX();
     const auto y = mouse.getMouseY();
-    for (auto w: widgets)
+    for (auto w : *widgets)
         w->invokePointerMove(x, y);
 }
 
-void GuiPointer::detectMouseButtons(const Input::Mouse& mouse)
+void GuiLayer::GuiPointer::detectMouseButtons(const Input::Mouse& mouse)
 {
-    const auto& pos = mouse.getMousePos();
+    const auto& pos           = mouse.getMousePos();
     Widget* underCursorWidget = getWidgetAtPoint(pos);
     for (uint16_t i = 0; i < NUM_MOUSE_BUTTONS; ++i) {
-        const auto oldState = mouseButtonsStates[i];
-        const auto newState = mouse.buttonDown(i);
+        const auto oldState   = mouseButtonsStates[i];
+        const auto newState   = mouse.buttonDown(i);
         mouseButtonsStates[i] = newState;
 
         if (newState == oldState)
@@ -234,32 +112,34 @@ void GuiPointer::detectMouseButtons(const Input::Mouse& mouse)
             underCursorWidget->invokeButtonPressed(i, pos.x, pos.y);
 
         if (!newState)
-            for (auto w: widgets)
+            for (auto w : *widgets)
                 w->invokeButtonReleased(i, pos.x, pos.y);
     }
 }
 
-void GuiPointer::processKeyboard(const Input::Keyboard& kbd)
+void GuiLayer::GuiPointer::processKeyboard(const Input::Keyboard& kbd)
 {
     const auto focus = Widget::getCurrentFocus();
-    if(!focus)
+    if (!focus)
         return;
 
-    for(const InputEvent& e: kbd.getEvents()) {
+    for (const InputEvent& e : kbd.getEvents()) {
         switch (e.type) {
-        case INPUT_EVENT_TYPE::KEY_DOWN: focus->invokeKeyDown(e.scancode); break;
+        case INPUT_EVENT_TYPE::KEY_DOWN:
+            focus->invokeKeyDown(e.scancode);
+            break;
         case INPUT_EVENT_TYPE::KEY_UP: focus->invokeKeyUp(e.scancode); break;
         default: assert("Shouldn't be here"); break;
         }
     }
 
-    if(kbd.hasText()) {
+    if (kbd.hasText()) {
         const auto& text = kbd.getText();
         focus->invokeTextEntered(text);
     }
 }
 
-void GuiPointer::processInput(const Input& input)
+void GuiLayer::GuiPointer::processInput(const Input& input)
 {
     const auto& mouse = input.getMouse();
     const auto& kbd   = input.getKeyboard();
--- gui/guilayer.h
+++ gui/guilayer.h
@@ -1,13 +1,14 @@
 #ifndef GUILAYER_H
 #define GUILAYER_H
 #include <memory>
+#include <vector>
+#include <vvv3d/vvv3d.h>
 #include <vvv3d/vvvmath/vector2.h>
 
 class Widget;
 class Camera;
 class Input;
-class GuiLayer final
-{
+class GuiLayer final {
 public:
     GuiLayer();
     ~GuiLayer();
@@ -24,12 +25,38 @@ public:
 
     void processInputEvents(const Input& input);
 
-    void setVisible(bool visisble);
+    void setVisible(bool visible);
     bool getVisible() const;
 
 private:
-    struct GuiLayerImpl;
-    std::unique_ptr<GuiLayerImpl> impl;
+    using WidgetsContainer = std::vector<Widget*>;
+
+    class GuiPointer {
+    public:
+        GuiPointer(WidgetsContainer& widgets) : widgets(&widgets) {}
+
+        void processInput(const Input& input);
+
+        Widget* getWidgetAtPoint(const vvv::vector2i& pos);
+
+        constexpr static uint16_t NUM_MOUSE_BUTTONS = 8;
+
+    private:
+        WidgetsContainer* widgets;
+        bool mouseButtonsStates[NUM_MOUSE_BUTTONS]{false};
+        void detectMouseMove(const Input::Mouse& mouse);
+        void detectMouseButtons(const Input::Mouse& mouse);
+        void processKeyboard(const Input::Keyboard& kbd);
+    };
+
+    WidgetsContainer widgets;
+    vvv::vector2<int> size;
+    /**
+     * @brief Orthographic camera; top-left corner (0,0)
+     */
+    Camera camera;
+    GuiPointer pointer;
+    bool visible;
 };
 
 #endif // GUILAYER_H
