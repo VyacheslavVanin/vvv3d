d0221b3ef808e28b7141b94460c31c86359ecad4
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2017-06-24 09:39:27 +0300

    embed sprite shader

--- sprites/sprite.cpp
+++ sprites/sprite.cpp
@@ -18,16 +18,52 @@ std::unique_ptr<Geometry> makeSpriteGeometry()
         GL_TRIANGLES);
 }
 
+static void loadSpriteShader()
+{
+    const char* fsh = R"(
+    #version 330 core
+    precision mediump float;
+    uniform sampler2D texture0;
+    uniform float time;
+
+    in  vec2 out_position;
+    in  vec2 out_texCoord;
+    out vec4 color;
+
+    void main(void)
+    {
+        color = texture2D( texture0, out_texCoord );
+    }
+    )";
+
+    const char* vsh = R"(
+    #version 330 core
+    in vec2 va_position;
+    in vec2 va_texCoord;
+    uniform mat4    modelMatrix;
+    uniform mat4    viewProjectionMatrix;
+    uniform vec4    texturePosition;
+    out highp vec2  out_texCoord;
+
+    void main(void)
+    {
+        gl_Position  = viewProjectionMatrix*modelMatrix*vec4(va_position, 0 ,1);
+        out_texCoord = texturePosition.xy + va_texCoord*texturePosition.zw;
+    }
+    )";
+    auto& e  = Engine::getActiveEngine();
+    auto& sm = e.getResourceManager().getShaderManager();
+    sm.addFromSource("sprite", vsh, fsh);
+}
+
 Sprite::Sprite() : transform(), texture(nullptr)
 {
     static std::once_flag flag;
     std::call_once(flag, []() {
         auto& resMan    = Engine::getActiveEngine().getResourceManager();
-        auto& shaderMan = resMan.getShaderManager();
         auto& geomMan   = resMan.getGeometryManager();
         geomMan.add("sprite", makeSpriteGeometry());
-        shaderMan.add("sprite", "data/shaders/sprite.vsh",
-                      "data/shaders/sprite.fsh");
+        loadSpriteShader();
     });
 }
 
