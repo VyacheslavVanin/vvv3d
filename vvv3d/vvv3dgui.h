05ed77f8cc22fc1667db4e0992938f069b966488
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2017-06-29 17:19:12 +0300

    Add vvv3dgui.h header

---
+++ vvv3d/vvv3dgui.h
@@ -0,0 +1,15 @@
+#ifndef VVV3DGUI_H
+#define VVV3DGUI_H
+#include <vvv3d/vvv3d.h>
+#include <vvv3d/gui/guilayer.h>  
+#include <vvv3d/gui/widget.h>  
+#include <vvv3d/gui/panel.h>  
+#include <vvv3d/gui/textwidget.h>  
+#include <vvv3d/gui/imagewidget.h>  
+#include <vvv3d/gui/colorrectanglewidget.h>  
+#include <vvv3d/gui/buttontext.h>  
+#include <vvv3d/gui/lineedit.h>  
+#include <vvv3d/gui/horizontallayout.h>  
+#include <vvv3d/gui/verticallayout.h>  
+
+#endif
