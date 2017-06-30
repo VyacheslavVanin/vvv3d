27025bc704740ff74ed417f2b4f7f8e10e2d4b93
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2017-06-30 13:30:00 +0300

    move gui dir to vvv3d/std/

--- vvv3d/vvv3dgui.h
+++ vvv3d/vvv3dgui.h
@@ -1,15 +1,15 @@
 #ifndef VVV3DGUI_H
 #define VVV3DGUI_H
 #include <vvv3d/vvv3d.h>
-#include <vvv3d/gui/guilayer.h>  
-#include <vvv3d/gui/widget.h>  
-#include <vvv3d/gui/panel.h>  
-#include <vvv3d/gui/textwidget.h>  
-#include <vvv3d/gui/imagewidget.h>  
-#include <vvv3d/gui/colorrectanglewidget.h>  
-#include <vvv3d/gui/buttontext.h>  
-#include <vvv3d/gui/lineedit.h>  
-#include <vvv3d/gui/horizontallayout.h>  
-#include <vvv3d/gui/verticallayout.h>  
+#include <vvv3d/std/gui/guilayer.h>
+#include <vvv3d/std/gui/widget.h>
+#include <vvv3d/std/gui/panel.h>
+#include <vvv3d/std/gui/textwidget.h>
+#include <vvv3d/std/gui/imagewidget.h>
+#include <vvv3d/std/gui/colorrectanglewidget.h>
+#include <vvv3d/std/gui/buttontext.h>
+#include <vvv3d/std/gui/lineedit.h>
+#include <vvv3d/std/gui/horizontallayout.h>
+#include <vvv3d/std/gui/verticallayout.h>
 
 #endif
