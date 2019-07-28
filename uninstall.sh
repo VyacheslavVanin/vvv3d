874c40824702a25c051baf5e4efb6ca3d51bb3d0
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2019-07-28 21:38:28 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo rm -rf /usr/include/vvv3d
 sudo rm -rf /usr/include/vvvcfg
+sudo rm -rf /usr/include/vvvstdhelper
 sudo rm -rf /usr/lib/libvvv3d.so
 sudo rm -rf /usr/lib/libvvvcfg.a
 sudo rm -rf /usr/lib/libvvvcfg.so
