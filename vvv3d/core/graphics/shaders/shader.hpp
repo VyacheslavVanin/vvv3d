d0c1fc2bd132a6d31cb685ed0adb92510cd5ab23
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2019-07-30 10:47:29 +0300

    Move ShaderManager to separate files

--- vvv3d/core/graphics/shaders/shader.hpp
+++ vvv3d/core/graphics/shaders/shader.hpp
@@ -116,35 +116,4 @@ public:
     explicit Shader(const _private&) : Shader() {}
 };
 
-class ShaderManager {
-public:
-    ShaderManager();
-
-    void add(const std::string& name, const std::string& vertexShaderFilename,
-             const std::string& fragmentShaderFilename);
-    void add(const std::string& name, const std::string& vertexShaderFilename,
-             const std::string& fragmentShaderFilename,
-             const std::string& geometryShaderFilename);
-    void add(const std::string& name, std::unique_ptr<Shader> shader);
-    void add(const std::string& name,
-             const std::function<std::unique_ptr<Shader>()>& f);
-    void addFromSource(const std::string& name,
-                       const std::string& vertexShaderSource,
-                       const std::string& fragmentShaderSource);
-    void addFromSource(const std::string& name,
-                       const std::string& vertexShaderSource,
-                       const std::string& fragmentShaderSource,
-                       const std::string& geometryShaderSource);
-
-    Shader& get(const std::string& name) const;
-    Shader& get(const std::string& name);
-    bool contain(const std::string& name) const;
-
-    std::vector<std::string> listNames() const;
-
-private:
-    std::unordered_map<std::string, std::unique_ptr<Shader>> shaders;
-    using init_func_t = std::function<std::unique_ptr<Shader>(void)>;
-    std::unordered_map<std::string, init_func_t> initializers;
-};
 } // namespace vvv3d
--- vvv3d/core/resourcemanager.hpp
+++ vvv3d/core/resourcemanager.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <vvv3d/core/graphics/fonts/font.hpp>
 #include <vvv3d/core/graphics/geometry.hpp>
-#include <vvv3d/core/graphics/shaders/shader.hpp>
+#include <vvv3d/core/graphics/managers/shader_manager.hpp>
 #include <vvv3d/core/graphics/textures/texturemanager.hpp>
 #include <vvv3d/std/sprites/animation.hpp>
 
